572a929e6718c418f2acd521d1c4cd32a8cf4c43
Poogal0 <golubchikov_2002@mail.ru>
2023-05-31 14:39:59 +0400

    v3 - created Red dot

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <QApplication>
 #include <QWidget>
-#include <QPushButton>
+#include <QPainter>
+#include <QTimer>
+
+class EllipseWidget : public QWidget {
+
+
+protected:
+    void paintEvent(QPaintEvent *) override {
+        QPainter painter(this);
+        painter.setBrush(Qt::red);
+        painter.setPen(Qt::NoPen);
+        painter.drawEllipse(m_x, height() / 2, 50, 50);
+    }
+
+
+
+private:
+    int m_x;
+};
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    QWidget window;
-    window.show();
+
+    EllipseWidget *widget = new EllipseWidget();
+    widget->setFixedSize(400, 200);
+    widget->show();
+
     return app.exec();
 }
