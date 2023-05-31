bc23cfbddeb27e0cc0bddbace7d87523dfb0db06
Poogal0 <golubchikov_2002@mail.ru>
2023-05-31 15:12:35 +0400

    3 v3 - added button "-"

--- main.cpp
+++ main.cpp
@@ -8,18 +8,22 @@ class FontSizeChanger : public QWidget {
 public:
     FontSizeChanger(QWidget *parent = nullptr) : QWidget(parent) {
         QLabel *label = new QLabel("Hello, World!", this);
-        QPushButton *plusButton = new QPushButton("+", this);        
+        QPushButton *plusButton = new QPushButton("+", this);
+        QPushButton *minusButton = new QPushButton("-", this);
         QLabel *sizeLabel = new QLabel(QString::number(label->font().pointSize()), this);
 
        
         QHBoxLayout *layout = new QHBoxLayout(this);
         layout->addWidget(label);
         layout->addWidget(plusButton);
-                layout->addWidget(sizeLabel);
+        layout->addWidget(minusButton);
+        layout->addWidget(sizeLabel);
 
         
         connect(plusButton, &QPushButton::clicked, this, &FontSizeChanger::increaseFontSize);
-           
+        connect(minusButton, &QPushButton::clicked, this, &FontSizeChanger::decreaseFontSize);
+
+   
         setFixedSize(600, 400);
     }
 
@@ -28,6 +32,16 @@ private slots:
         QLabel *label = findChild<QLabel *>();
         QFont font = label->font();
         font.setPointSize(font.pointSize() + 1);
+        label->setFont(font);
+
+        QLabel *sizeLabel = findChild<QLabel *>(QString(), Qt::FindDirectChildrenOnly);
+        sizeLabel->setText(QString::number(font.pointSize()));
+    }
+
+    void decreaseFontSize() {
+        QLabel *label = findChild<QLabel *>();
+        QFont font = label->font();
+        font.setPointSize(font.pointSize() - 1);
         label->setFont(font);
 
         QLabel *sizeLabel = findChild<QLabel *>(QString(), Qt::FindDirectChildrenOnly);
